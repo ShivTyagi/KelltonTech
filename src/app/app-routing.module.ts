abfcf794910930154851a78c75947b5b4db10e9b
aditya <aditya.tyagi@rsystems.com>
2021-09-02 11:05:15 +0530

    ui-ux-changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { AnagramComponent } from './anagram/anagram.component';
+import { TempTrackerComponent } from './temp-tracker/temp-tracker.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : 'registration', component : RegisterComponent},
+  {path : 'anagram', component : AnagramComponent},
+  {path : 'tempTracker', component : TempTrackerComponent},
+  { path: '',   redirectTo: '/registration', pathMatch: 'full' }, // redirect to 
+  { path: '**', component: RegisterComponent },  // Wildcard route 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
