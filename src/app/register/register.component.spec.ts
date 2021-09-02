a3510f9ef65b56dcb1515dd20f89c3ece1fb419a
aditya <aditya.tyagi@rsystems.com>
2021-09-02 15:25:16 +0530

    changes

--- src/app/register/register.component.spec.ts
+++ src/app/register/register.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
-describe('RegisterComponent', () => {
+fdescribe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ RegisterComponent ],
+      imports : [ReactiveFormsModule]
     })
     .compileComponents();
   });
@@ -22,4 +24,45 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('first Name field validity', () => {
+    let name = component.registerForm.controls['firstName'];
+    // expect(name.valid).toBeFalsy();
+    name.setValue("");
+    expect(name.valid).toBeFalsy();
+    name.setValue("AAA");
+    expect(name.hasError('minlength')).toBeTruthy();
+     
+});
+
+it('LastName field validity', () => {
+  let lastName = component.registerForm.controls['lastName'];
+  // expect(name.valid).toBeFalsy();
+  lastName.setValue("");
+  expect(lastName.valid).toBeFalsy();
+  lastName.setValue("BBB");
+  expect(lastName.hasError('minlength')).toBeTruthy();
+   
+});
+
+it('country field validity', () => {
+  let country = component.registerForm.controls['country'];
+  // expect(name.valid).toBeFalsy();
+  country.setValue("");
+  expect(country.valid).toBeFalsy();
+   
+});
+
+it('phone number field validity', () => {
+  let phoneNumber = component.registerForm.controls['phoneNumber'];
+  // expect(name.valid).toBeFalsy();
+  phoneNumber.setValue("");
+  expect(phoneNumber.valid).toBeFalsy();
+
+  phoneNumber.setValue("AAA");
+  expect(phoneNumber.hasError('pattern')).toBeTruthy();
+   
+});
+
+
 });
